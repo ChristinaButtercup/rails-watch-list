41d7d42625b225ec19c810b263a28b0f90efcfc2
Christina Borensky <christina.borensky@web.de>
2022-02-17 16:16:29 +0100

    Controller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+end
